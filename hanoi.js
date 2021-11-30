8c5b62e3154b0a288b916dea5db402859430add5
Carlos Roberto <carlos.rrds@gmail.com>
2021-11-30 12:11:59 -0300

    criando discos via js

--- hanoi.js
+++ hanoi.js
@@ -11,3 +11,13 @@ torreEnd.setAttribute("id", "torreEnd");
 torres.appendChild(torreStart);
 torres.appendChild(torreOffset);
 torres.appendChild(torreEnd);
+
+const disco1 = document.createElement('div')
+disco1.setAttribute('id', 'disco1')
+torreStart.appendChild(disco1)
+const disco2 = document.createElement('div')
+disco2.setAttribute('id', 'disco2')
+torreStart.appendChild(disco2)
+const disco3 = document.createElement('div')
+disco3.setAttribute('id', 'disco')
+torreStart.appendChild(disco3)
