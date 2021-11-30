a454566696ba037238c141c2cb33a352e411009e
Raphael <macedolima22@hotmail.com>
2021-11-30 12:07:47 -0300

    criando torres via js

---
+++ hanoi.js
@@ -0,0 +1,13 @@
+const torres = document.getElementById('torres');
+//torres
+const torreStart = document.createElement('div');
+const torreOffset = document.createElement('div');
+const torreEnd = document.createElement('div');
+
+torreStart.setAttribute("id", "torreStart");
+torreOffset.setAttribute("id", "torreOffset");
+torreEnd.setAttribute("id", "torreEnd");
+
+torres.appendChild(torreStart);
+torres.appendChild(torreOffset);
+torres.appendChild(torreEnd);
